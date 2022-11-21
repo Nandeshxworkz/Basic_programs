3104deb0cb08cef8af3a36b1c44c6de8fecea51a
Nandeshxworkz <nandeshchannal.xworkz@gmail.comgit push origin maingit config --global user.email nandeshchannal.xworkz@gmail.com>
2022-11-21 14:32:38 +0530

    Star pattern programs

---
+++ basicprograms/src/com/xworkz/starpatternprograms/ThirdQuadrantStarPattern.java
@@ -0,0 +1,19 @@
+package com.xworkz.starpatternprograms;
+
+public class ThirdQuadrantStarPattern {
+
+	public static void main(String[] args) {
+
+		for(int i=1; i<=5; i++) {
+			for(int j=5; j>=i;j--) {
+				System.out.print("*");
+			}
+			for(int k=1; k<=5;k++) {
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
