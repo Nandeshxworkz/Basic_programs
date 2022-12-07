d8c208e5b2d588173949a22a8731691cc7d22486
Nandeshxworkz <nandeshchannal.xworkz@gmail.comgit push origin maingit config --global user.email nandeshchannal.xworkz@gmail.com>
2022-12-07 14:08:32 +0530

    test

---
+++ basicprograms/src/com/xworkz/Test/StringInsert.java
@@ -0,0 +1,12 @@
+package com.xworkz.Test;
+
+public class StringInsert {
+
+	public static void main(String[] args) {
+		String input = "Computer Portal";
+
+		String concat = input.concat(" ").concat("Science");
+		System.out.println(concat);
+	}
+
+}
