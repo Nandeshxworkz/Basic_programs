65dce8dac6c032e756f47d64cf51fb99f25533ab
Nandeshxworkz <nandeshchannal.xworkz@gmail.comgit push origin maingit config --global user.email nandeshchannal.xworkz@gmail.com>
2022-12-08 15:39:41 +0530

    Test

--- basicprograms/src/com/xworkz/Test/StringInsert.java
+++ basicprograms/src/com/xworkz/Test/StringInsert.java
@@ -4,9 +4,14 @@ public class StringInsert {
 
 	public static void main(String[] args) {
 		String input = "Computer Portal";
-
-		String concat = input.concat(" ").concat("Science");
-		System.out.println(concat);
+        String insert="Science ";
+        
+        int index=8;
+        
+        StringBuffer sb = new StringBuffer(input);
+        StringBuffer insert2 = sb.insert(index+1, insert);
+        System.out.println(insert2);
+		
 	}
 
 }
